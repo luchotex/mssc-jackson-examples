984dd9b1fede7c6f6fc1cfc5cc2ae470d5a03686
Luis Kupferberg <lkupferberg@overactive.com>
2020-07-16 23:17:22 -0400

    -- Added the beerDto snake case naming strategy.

---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoSnakeTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ * @author Luis M. Kupferberg Ruiz (lkupferberg@overactive.com)
+ * @created 2020-07-16 23:11
+ */
+@ActiveProfiles("snake")
+@JsonTest
+public class BeerDtoSnakeTest extends BaseTest {
+
+  @Autowired private ObjectMapper objectMapper;
+
+  @Test
+  void snakeTest() throws JsonProcessingException {
+    BeerDto beerDto = getDto();
+
+    String jsonString = objectMapper.writeValueAsString(beerDto);
+
+    System.out.println(jsonString);
+  }
+}
